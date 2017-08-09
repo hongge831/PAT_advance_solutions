b73fd23cbf4a21ee108bacfe1d9cd1fb56275112
hongge831 <541324951@qq.com>
2017-08-09 18:42:20 +0800

    modify 1016

--- pat_advance_1016.cpp
+++ pat_advance_1016.cpp
@@ -1,121 +1,74 @@
 #include <stdio.h>
 #include <iostream>
-#include <string>
-#include <string.h>
 #include <vector>
+#include <map>
+#include <string>
 #include <algorithm>
-#include <stdlib.h>
 using namespace std;
-struct info{
+int toll[25] = { 0 };//the additional one refer to the sum of others
+struct data{
 	string name;
-	string time;
-	string status;
+	int month, day, hour, min, status, totalTime;
 };
-
-void initToll(int toll[]){
+bool cmp(data a, data b){
+	return a.name != b.name ? a.name < b.name : a.totalTime < b.totalTime;
+}
+float computeMoney(data a){
+	float re = 0.0;
+	re += a.day * 60 * toll[24] + toll[a.hour] * a.min;
+	for (int i = 0; i < a.hour; i++)
+	{
+		re += toll[i]*60;
+	}
+	return re / 100;
+}
+int main(){
+	/*initialize the toll arr*/
 	for (int i = 0; i < 24; i++)
 	{
 		cin >> toll[i];
+		toll[24] += toll[i];
 	}
-}
-void inputV(vector<info> &v){
-	for (int i = 0; i < v.size(); i++)
+	/*input the information of customer's active*/
+	int n;
+	string temp;
+	cin >> n;
+	vector<data> v(n);
+	for (int i = 0; i < n; i++)
 	{
 		cin >> v[i].name;
-		cin >> v[i].time;
-		cin >> v[i].status;
+		scanf_s("%d:%d:%d:%d", &v[i].month, &v[i].day, &v[i].hour, &v[i].min);
+		v[i].totalTime = v[i].day * 24 * 60 + v[i].hour * 60 + v[i].min;
+		cin >> temp;
+		v[i].status = temp == "on-line" ? 1 : 0;
 	}
-}
-//this template function must have two return result
-bool cmp(const info &a, const info &b){
-	if (strcmp(a.name.c_str(), b.name.c_str()) > 0){
-		return true;
-	}
-	else if (strcmp(a.name.c_str(), b.name.c_str()) == 0){
-		if (strcmp(a.time.c_str(), b.time.c_str()) < 0){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	else{
-		return false;
-	}
-		
-}
-bool cmp2(const info &a, const info &b){
-	if (strcmp(a.name.c_str(), b.name.c_str()) < 0){
-		return true;
-	}
-	else if (strcmp(a.name.c_str(), b.name.c_str()) == 0){
-		if (strcmp(a.time.c_str(), b.time.c_str()) < 0){
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	else{
-		return false;
-	}
-
-}
-int computeDT(int dd,int hour,int min,int dd2,int hour2,int min2){
-	return (dd2 - dd) * 24 * 60 + (hour2 - hour) * 60 + min2 - min;
-
-}
-float computeMN(int toll[], int dd, int hour, int min, int dd2, int hour2, int min2){
-	return 0.0;
-}
-int main(){
-	int toll[24];
-	initToll(toll);
-	int n; cin >> n;
-	vector<info> v(n);
-	vector<info> v2;
-	inputV(v);
-	sort(v.begin(),v.end(),cmp);
-	int bef = n-1, last = n-1;
-	int month, dd, hour, min, month2, dd2, hour2, min2;
-	while (bef > 0){
-		while (v[last].status != "off-line" && last>0){
-			last--;
-		}bef = last - 1;
-		if (bef>0&&v[bef].name == v[last].name && v[bef].status == "on-line"){
-			v2.push_back(v[bef]);
-			v2.push_back(v[last]);
-			last = bef - 1;
-		}
-		else{
-			bef--;
-		}
-	}sort(v2.begin(), v2.end(), cmp2);
-	int diffmin;
-	float sum = 0.0,money=0;
-	bool flag = true;
-	for (int i = 0; i < v2.size(); i+=2)
+	/*sort the customer's arr*/
+	sort(v.begin(), v.end(), cmp);
+	/*select the right combination insert the map*/
+	map<string, vector<data>> mp;
+	map<string, vector<data>>::iterator it;
+	for (int i = 1; i < n; i++)
 	{
-		sscanf_s(v2[i].time.c_str(), "%d:%d:%d:%d", &month, &dd, &hour, &min);
-		sscanf_s(v2[i+1].time.c_str(), "%d:%d:%d:%d", &month2, &dd2, &hour2, &min2);
-		diffmin = computeDT(dd,hour,min,dd2,hour2,min2);
-		money = computeMN(toll, dd, hour, min, dd2, hour2, min2);
-		sum += money;
-		if (flag){
-			printf("%s %02d\n", v2[i].name.c_str(), month);
-			flag = false;
-		}
-		
-		printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n", dd, hour, min, dd2, hour2, min2, diffmin, money);
-		if ((i + 2) < v2.size()){
-			if (v2[i + 2].name != v2[i].name){
-				printf("Total amount: %.2f\n", sum);
-				flag = true;
-			}
+		if (v[i - 1].name == v[i].name &&v[i - 1].status == 1 && v[i].status == 0){
+			mp[v[i - 1].name].push_back(v[i - 1]);
+			mp[v[i - 1].name].push_back(v[i]);
 		}
 	}
+	float t = 0.0, totalMoney = 0.0;
+	int time;
+	for (it = mp.begin(); it != mp.end(); it++)
+	{
+		printf("%s %02d\n", it->first.c_str(), it->second[0].month);
+		for (int i = 0; i < it->second.size(); i += 2)
+		{
+			time = it->second[i + 1].totalTime - it->second[i].totalTime;
+			t = computeMoney(it->second[i + 1]) - computeMoney(it->second[i]);
+			printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n", it->second[i].day, it->second[i].hour, it->second[i].min, it->second[i + 1].day, it->second[i + 1].hour, it->second[i + 1].min, time, t);
+			totalMoney += t;
+		}
+		printf("Total amount: $%.2f\n", totalMoney);
+		totalMoney = 0.0;
 
-
-	
+	}
 	return 0;
 }
