ace987cd00a2ea04707b8c0f03e6c6d210e109f0
hongge831 <541324951@qq.com>
2017-08-16 22:32:29 +0800

    1099

---
+++ pat_advance_1093.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	int patCount = 0, atCount = 0, tCount = 0;
+	string str;
+	cin >> str;
+	for (int i = str.size() - 1; i >= 0; i--)
+	{
+		if (str[i] == 'T'){
+			tCount++;
+		}
+		if (str[i] == 'A'){
+			atCount = (atCount + tCount) % 1000000007;
+		}
+		if (str[i] == 'P'){
+			patCount = (patCount + atCount) % 1000000007;
+		}
+	}cout << patCount << endl;
+	return 0;
+}
