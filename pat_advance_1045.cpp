478f4d58561869a312a3aa9f62c527f8c3d06841
hongge831 <541324951@qq.com>
2017-09-01 16:09:32 +0800

    add 1045

---
+++ pat_advance_1045.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int color[201];
+vector<int> v,dp;
+int main(){
+	int n, m, l;
+	cin >> n >> m;
+	int temp;
+	for (int i = 1; i <= m; i++)
+	{
+		scanf_s("%d",&temp);
+		color[temp] = i;
+	}
+	cin >> l;
+	for (int i = 0; i < l; i++)
+	{
+		scanf_s("%d",&temp);
+		if (color[temp] != 0){
+			v.push_back(color[temp]);
+		}
+	}
+	/*dynamic procedure begin*/
+	dp.resize(v.size());
+	int ans = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		dp[i] = 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (v[i] >= v[j]){
+				dp[i] = max(dp[j]+1,dp[i]);
+			}
+		}
+		ans = max(ans,dp[i]);	
+	}
+	printf("%d",ans);
+	return 0;
+}
